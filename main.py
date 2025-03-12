4eeae35d2a1bd44c62ca6baef7a5ab299d49f135
g <nuaalhd@163.com>
2025-03-12 16:09:46 +0800

    增加推送

--- main.py
+++ main.py
@@ -84,4 +84,4 @@ for section in configs.sections():
 
 # 推送消息
 send_message.send_server_chan(config.SCKEY, s_title, s_content)
-send_message.send_pushplus(config.PUSHPLUS_TOKEN, s_title, s_content)
+send_message.send_pushplus(config.PUSH_TOKEN, s_title, s_content)
