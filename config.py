1fcd3478dc356910a91d8ebc26bf86567b7ab3c3
g <nuaalhd@163.com>
2025-03-12 15:26:19 +0800

    OK

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 如没有配置则不推送消息
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
-PUSH_TOKEN = os.environ.get("954c339092254f8fbc837686f7e290bf")
+PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
 
 # server酱 微信推送。使用参考 https://sct.ftqq.com/
 SCKEY = os.environ.get('SCKEY')
@@ -29,7 +29,7 @@ SCKEY = os.environ.get('SCKEY')
 *********** 地图配置 ***********
 获取地点信息,这里用的高德api,需要自己去高德开发者平台申请自己的key
 '''
-AMAP_KEY = "1fd2993cce6a61e086e65e201248c4bf"
+AMAP_KEY = os.environ.get("GAODE_KEY")
 
 
 '''
